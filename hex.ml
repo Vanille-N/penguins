88fc66c3d8b0a1bdeb6fbb21314561bf2f6aa5a6
Vanille-N <neven.villani@gmail.com>
2021-04-04 08:28:09 +0200

    hex: read grid

--- hex.ml
+++ hex.ml
@@ -39,3 +39,58 @@ let pp_grid fmt gr =
         fprintf fmt "\n"
     )) gr
 
+let read_grid chan interprete =
+    let lines = ref [] in
+    (try
+        (* seek start *)
+        let line = ref "" in
+        while !line <> "<problem>" do line := input_line chan done;
+        (* read body *)
+        while !line <> "</problem>" do
+            line := input_line chan;
+            lines := !line :: !lines
+        done;
+        close_in chan
+    with End_of_file -> failwith "End of file before termination marker"
+    );
+    (* determine size of problem *)
+    let rec size (i, j) = function
+        | [] -> (i, j)
+        | line :: rest -> size (i + 1, max (String.length line / 2) j) rest
+    in
+    let lines = List.rev (List.tl !lines) in (* remove ending + reorder *)
+    let (imax, jmax) = size (0, 0) lines in
+    let (imax, jmax) = (imax + 2, jmax + 3) in (* padding *)
+    let gr = Array.init imax (fun _ -> Array.make jmax (interprete ' ')) in
+    (* determine starting point and hex offset *)
+    let (start, offset) = (
+        let find_start line = String.index_from_opt line 0 '#' in
+        let rec aux i = function
+            | [] -> failwith "This problem has no starting position"
+            | line :: rest -> match find_start line with
+                | None -> aux (i+1) rest
+                | Some j -> ((i, j), (i + j) mod 2)
+        in aux 0 lines
+    ) in
+    List.iteri (fun i line -> (
+        String.iteri (fun j c -> (
+            if (i + j + offset) mod 2 == 1 then (
+                if c <> ' ' then failwith (
+                    Format.sprintf "Position (%d,%d) should be empty" i j
+                )
+            ) else (
+                (* possibly put an ice block *)
+                    let target_i = i + 1 in
+                    let target_j = j / 2 + 1 + if offset = 0 then (
+                        0
+                    ) else (
+                        target_i mod 2
+                    ) in
+                    gr.(target_i).(target_j) <- interprete c
+            )
+        )) line
+    )) lines;
+    (start, gr)
+
+let from_channel chan = read_grid chan (fun c -> c <> ' ')
+let show_problem chan = read_grid chan (fun c -> c)
