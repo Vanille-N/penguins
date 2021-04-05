ae3c29f38fea528b51130be89490349dead790ea
Vanille-N <neven.villani@gmail.com>
2021-04-05 08:26:49 +0200

    make node have mutable key

--- priority.ml
+++ priority.ml
@@ -4,7 +4,10 @@ module type ORDERED = sig
 end
 
 module Make (M:ORDERED) = struct
-    type 'a node = M.t * 'a
+    type 'a node = {
+        mutable key: M.t;
+        value: 'a;
+    }
     type 'a queue = {
         mutable size: int;
         contents: 'a node array;
@@ -12,27 +15,27 @@ module Make (M:ORDERED) = struct
 
     let create max_size init_key init_value = {
         size = 0;
-        contents = Array.make max_size (init_key, init_value);
+        contents = Array.make max_size { key=init_key; value=init_value; };
     }
     
     let size q = q.size
 
     let sift q n =
-        let (key, elem) = q.contents.(n) in
+        let node = q.contents.(n) in
         let rec aux n =
             if n > 0 then (
                 let father = (n - 1) / 2 in
-                if M.compare (fst q.contents.(father)) key > 0 then (
+                if M.compare q.contents.(father).key node.key > 0 then (
                     q.contents.(n) <- q.contents.(father);
                     aux father
                 ) else n
             ) else 0
         in
         let new_pos = aux n in
-        q.contents.(new_pos) <- (key, elem)
+        q.contents.(new_pos) <- node
 
     let rec trinkle q n =
-        let (key, elem) = q.contents.(n) in
+        let node = q.contents.(n) in
         let rec aux n =
             let base = 2 * n + 1 in
             let son = if base >= q.size then (
@@ -40,8 +43,8 @@ module Make (M:ORDERED) = struct
             ) else if base + 1 = q.size then (
                 Some base
             ) else if M.compare
-                (fst q.contents.(base))
-                (fst q.contents.(base + 1))
+                q.contents.(base).key
+                q.contents.(base + 1).key
                 <= 0 then (
                 Some base
             ) else (
@@ -50,8 +53,8 @@ module Make (M:ORDERED) = struct
                 | None -> n
                 | Some s -> (
                     if M.compare
-                        key
-                        (fst q.contents.(s))
+                        node.key
+                        q.contents.(s).key
                         > 0 then (
                         q.contents.(n) <- q.contents.(s);
                         aux s
@@ -59,10 +62,10 @@ module Make (M:ORDERED) = struct
                 )
         in
         let new_pos = aux n in
-        q.contents.(new_pos) <- (key, elem)
+        q.contents.(new_pos) <- node
         
     let insert q key value =
-        let node = (key, value) in
+        let node = { key=key; value=value; } in
         q.contents.(q.size) <- node;
         q.size <- q.size + 1;
         sift q (q.size - 1);
@@ -78,15 +81,14 @@ module Make (M:ORDERED) = struct
         );
         node
     
-    let key = fst
-    let value = snd
+    let key node = node.key
+    let value node = node.value
 
     let find q node =
-        let (key, value) = node in
         let rec explore n =
             if n >= q.size then None
             else if q.contents.(n) = node then Some n
-            else if M.compare (fst q.contents.(n)) key > 0 then None
+            else if M.compare q.contents.(n).key node.key > 0 then None
             else match explore (2 * n + 1) with
                 | None -> explore (2 * n + 2)
                 | Some n -> Some n
@@ -108,9 +110,13 @@ module Make (M:ORDERED) = struct
         )
 
     let decrease_key q node new_key = match find q node with
-        | None -> let _ = insert q new_key (value node) in ()
+        | None -> (
+            node.key <- new_key;
+            let _ = insert q new_key (value node) in ()
+        )
         | Some n -> (
-            q.contents.(n) <- (new_key, value node);
+            q.contents.(n).key <- new_key;
+            node.key <- new_key;
             sift q n
         )
 end
