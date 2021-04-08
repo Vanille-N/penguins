620cf1e94ea7a4b3e1afb058315767950ec61443
Vanille-N <neven.villani@gmail.com>
2021-04-08 11:10:41 +0200

    main()

--- main.ml
+++ main.ml
@@ -1,4 +1,13 @@
+let (start, grid) = Hex.from_channel stdin 
+
+module M : (Paths.S) = struct
+    let grid = grid
+end
+
+module Path = Paths.Make(M)
+
 let () =
-    let grid = Hex.show_problem stdin in
-    Hex.pp_grid Format.std_formatter (snd grid)
+    let (len, moves) = Path.maxpath start in
+    let path = Hex.path_of_moves start moves in
+    Path.pp_path Format.std_formatter path
 
