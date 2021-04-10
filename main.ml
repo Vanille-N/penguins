46fadb71fa464b896efa79be6a7f83c7419bb578
Vanille-N <neven.villani@gmail.com>
2021-04-10 11:38:59 +0200

    skeleton for argument parsing

--- main.ml
+++ main.ml
@@ -1,12 +1,64 @@
+let input = ref stdin
+
+type kind = Optimize | Display | Help | File
+type arg = End | Error of string | Arg of kind * string
+
+let print_help =
+    List.iter (function
+        | Optimize -> Format.printf "opt"
+        | Display -> Format.printf "disp"
+        | Help -> Format.printf "help"
+        | File -> Format.printf "file"
+    )
+
+let arg_interprete kind flag =
+    match kind with
+        | Optimize -> ()
+        | Display -> ()
+        | Help -> ()
+        | File -> ()
+
 let () =
-    Arg.(parse
-        [("--debug", Unit (fun () -> Config.debug := true), "turn on debug printing");
-         ("--display", Unit (fun () -> Config.display := true), "show grid at each step");
-         ("--ansi", Unit (fun () -> Config.ansi_fmt := true), "use ANSI escape codes to improve formatting");
-    ] (fun s -> ()) "Don't touch my Fish!\nPlease provide the input in stdin")
+    (* Parse command line arguments *)
+    let next = (
+        let i = ref 1 in
+        let len = Array.length Sys.argv in
+        let next () =
+            if !i >= len then End
+            else (
+                let arg = Sys.argv.(!i) in
+                incr i;
+                let n = String.length arg in
+                if n = 0 then Error "Zero-length argument is invalid"
+                else if arg.[0] <> '-' then Arg (File, arg)
+                else if n = 1 then failwith "'-' is not a valid argument"
+                else (
+                    let rest = String.sub arg 2 (n-2) in
+                    match arg.[1] with
+                        | 'o' -> Arg (Optimize, rest)
+                        | 'd' -> Arg (Display, rest)
+                        | 'h' -> Arg (Help, rest)
+                        | c -> Error (Format.sprintf "'%c' is not a valid argument type" c)
+                )
+            )
+        in next
+    ) in
+    let rec parse () =
+        match next () with
+            | End -> ()
+            | Error msg -> (
+                Format.printf "%s\n" msg;
+                print_help [Optimize; Display; Help; File]
+            )
+            | Arg (kind, flags) -> (
+                arg_interprete kind flags;
+                parse ()
+            )
+    in
+    parse ();
+    failwith "End"
 
-
-let (start, grid) = Hex.from_channel stdin 
+let (start, grid) = Hex.from_channel !input
 
 module M : (Paths.S) = struct
     let grid = grid
